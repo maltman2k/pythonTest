18ed2ee374409e72e2b3d34b1a6954d1d3f8efe3
maltman2K <maltman2k@hotmail.com>
2018-10-13 23:33:21 +0200

    Add existing file

---
+++ OneDrive - Johnson Controls/Develop/Python/rest.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import time
+import warnings
+
+
+warnings.filterwarnings("ignore")
+
+proxies = {
+  'http': '165.225.86.36:10319',
+  'https': '165.225.86.36:10319',
+}
+headers = {'accept' : 'application/json'}
+#url = 'https://sisedemos.ddns.net:9090/services/ede/virtualdevices/1'
+#response = requests.get('http://jsonplaceholder.typicode.com/todos/1', proxies=proxies,verify=False)
+#response = requests.get('https://sisedemos.ddns.net:9090/services/ede/virtualdevices/1/items/Room Temperature/value', proxies=proxies,verify=False,headers=headers)
+
+i = 1
+while i < 5:
+  i += 1
+  response = requests.get('https://sisedemos.ddns.net:9090/services/ede/items/Room Temperature/value', proxies=proxies,verify=False,headers=headers)
+  y = json.loads(response.text)
+  print (y[u'item'][u'value'])
+  time.sleep(2)
+
+
