568c49ed7ac086b6f93eef162fbbd2ac97165892
maltman2K <maltman2k@hotmail.com>
2018-10-14 09:29:04 +0200

    Add existing file

--- OneDrive - Johnson Controls/Develop/Python/rest.py
+++ OneDrive - Johnson Controls/Develop/Python/rest.py
@@ -16,11 +16,24 @@ headers = {'accept' : 'application/json'}
 #response = requests.get('https://sisedemos.ddns.net:9090/services/ede/virtualdevices/1/items/Room Temperature/value', proxies=proxies,verify=False,headers=headers)
 
 i = 1
+proxyEnabled=0;
+
+try:
+    response = requests.get('https://sisedemos.ddns.net:9090/services/ede/items/Room Temperature/value', proxies=proxies,verify=False,headers=headers)
+except IOError:
+    proxyEnabled=1;
+finally:
+    print ("success")
+
 while i < 5:
-  i += 1
-  response = requests.get('https://sisedemos.ddns.net:9090/services/ede/items/Room Temperature/value', proxies=proxies,verify=False,headers=headers)
-  y = json.loads(response.text)
-  print (y[u'item'][u'value'])
-  time.sleep(2)
+    if proxyEnabled==0:
+        response = requests.get('https://sisedemos.ddns.net:9090/services/ede/items/Room Temperature/value', proxies=proxies,verify=False,headers=headers)
+    else: 
+        response = requests.get('https://sisedemos.ddns.net:9090/services/ede/items/Room Temperature/value',verify=False,headers=headers)
+        
+    y = json.loads(response.text)
+    print (y[u'item'][u'value'])
+    time.sleep(2)
+    i += 1
 print ("end process execution")
 
